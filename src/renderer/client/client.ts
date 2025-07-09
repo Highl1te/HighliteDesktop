8a36d47037d036ed21a669d07253d625be08a8a7
Skye Brady <skye@blueskye.co.uk>
2025-07-09 20:12:41 +0100

    feat: new plugin to insert emoji into chat

--- src/renderer/client/client.ts
+++ src/renderer/client/client.ts
@@ -20,6 +20,7 @@ import { TreasureMapHelper } from './highlite/plugins/TreasureMapHelper';
 import { FPSLimiter } from './highlite/plugins/FPSLimiter';
 import { DefinitionsPanel } from './highlite/plugins/DefinitionsPanel';
 import { MinimapIcons } from './highlite/plugins/MinimapIcons';
+import { EmojiChat } from './highlite/plugins/EmojiChat';
 import { setupWorldSelectorObserver } from './helpers/worldSelectHelper';
 
 import '@static/css/index.css';
@@ -55,8 +56,9 @@ const PLUGIN_REGISTRY = [
     { class: XPOrb, path: './highlite/plugins/XPOrb' },
     { class: TreasureMapHelper, path: './highlite/plugins/TreasureMapHelper' },
     { class: FPSLimiter, path: './highlite/plugins/FPSLimiter' },
-    { class: DefinitionsPanel, path: './highlite/plugins/DefinitionsPanel' },
-    { class: MinimapIcons, path: './highlite/plugins/MinimapIcons' },
+    { class: DefinitionsPanel, path: './highlite/plugins/DefinitionsPanel'},
+    { class: MinimapIcons, path: './highlite/plugins/MinimapIcons'},
+    { class: EmojiChat, path: './highlite/plugins/EmojiChat'}
 ];
 
 async function obtainGameClient() {
